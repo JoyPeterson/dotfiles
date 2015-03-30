a4409d534f2adf5ef3106934626cd1241e6431aa
Joy Peterson <jpeterson@insidesales.com>
2015-03-30 13:43:43 -0600

    Combine functions for pulling uncommitted changes into a single more general purpose function.

--- bashlib/vision.bash
+++ bashlib/vision.bash
@@ -7,45 +7,26 @@ function vsto_upd() {
     cp /c/Projects/Outlook/VSTOContrib/src/VSTOContrib.Outlook/bin/Debug/VSTOContrib.*.* $et/plugins/outlook-vision/packages/VSTOContrib.Outlook.0.15.0.122/lib/net40/
 }
 
-# Copy modifiedfiles from an ihance repository to the ihance VM
-function cp_vision() {
-	src=$(pwd)
-	target=${src/\/c\/Projects\/ihance/\/s}
-
-	git st | \
-	awk -v src=$src -v target=$target \
-		'{printf "cp -v \"%s/%s\" \"%s/%s\"\n", src, $2, target, $2}' | sh
-}
-
 # Compile the vision binaries on the VM
 function compile_vision() {
 	ssh Administrator@jpeterson-ihanc "cd /c/src/source_home/web && src/compile.bash src"
 }
 
-# Copy ihance DLLs from the ihance VM to the local web repository
-function cp_web_bin() {
-	pushd /c/Projects/ihance/IHWeb > /dev/null
-	cp -v /s/web/appserver/site/bin/Ihance.dll appserver/site/bin/
-	cp -v /s/web/appserver/site/bin/Ihance.pdb appserver/site/bin/
-	cp -v /s/web/logger/site/bin/Ihance.dll logger/site/bin/
-	cp -v /s/web/logger/site/bin/Ihance.pdb logger/site/bin/
-	cp -v /s/web/src/bin/Ihance.dll src/bin/
-	cp -v /s/web/src/bin/Ihance.pdb src/bin/
-	cp -v /s/web/website/site/bin/Ihance.dll website/site/bin/
-	cp -v /s/web/website/site/bin/Ihance.pdb website/site/bin/
-	popd > /dev/nu
-}
-
-# Copy modified files from $src/web on the ihance vm to the local repository folder
-function cp_web() {
-	ssh Administrator@jpeterson-ihanc "cd /c/src/source_home/web && git st" \
-	| awk '{printf "scp Administrator@jpeterson-ihanc:/c/src/source_home/web/%s /c/Projects/ihance/IHWeb/%s\n", $2, $2}' \
-	| sh
-}
-
-# Copy modified files from $sit on the ihance vm to the local repository folder
-function cp_sit() {
-	ssh Administrator@jpeterson-ihanc "cd /c/src/source_home/sit && git st" \
-	| awk '{printf "scp Administrator@jpeterson-ihanc:/c/src/source_home/sit/%s /c/Projects/ihance/IHSit/%s\n", $2, $2}' \
-	| sh
+# Copy modified files from an ihance VM to the local repository folder.
+function cp_vision() {
+	remote_folder=$1
+	if [ "$remote_folder" != "web" ] && [ "$remote_folder" != "sit" ] && [ "$remote_folder" != "lib" ]; then
+		echo "Usage: cp_vision (web|sit|lib)"
+		return
+	fi
+	local_folder="IH${remote_folder}"
+	ssh Administrator@jpeterson-ihanc "cd /c/src/source_home/$remote_folder && git st" \
+	| awk '{printf "--include \"%s\" ", $2}' \
+	| awk -v remote_folder=$remote_folder -v local_folder=$local_folder \
+	  '{printf "rsync -amv %s--include \"*/\" --exclude=\"*\" \
+	  Administrator@jpeterson-ihanc:/c/src/source_home/%s/ \
+	  /c/Projects/ihance/%s/", \
+	  $0, remote_folder, local_folder}' \
+	| sh \
+	| grep -v /$ # Do not display directory names.
 }
